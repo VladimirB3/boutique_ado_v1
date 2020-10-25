5d821f7d3b8b50cb9ef8daa049900ff68aafef23
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-10-25 18:08:38 +0000

    Payment system completed

--- checkout/models.py
+++ checkout/models.py
@@ -6,7 +6,6 @@ from django.conf import settings
 
 from products.models import Product
 
-# Create your models here.
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
@@ -23,7 +22,8 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
@@ -55,7 +55,6 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_number
-
 
 
 class OrderLineItem(models.Model):
